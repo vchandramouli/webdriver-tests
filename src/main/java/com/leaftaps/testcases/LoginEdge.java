cb3849ab91851e508c9ac09e6693cf7ac2276aa8
Mohan Balakrishnan <38653431+mohan-balakrishnan@users.noreply.github.com>
2024-10-20 12:36:14 +0530

    Update LoginEdge.java

--- src/main/java/com/leaftaps/testcases/LoginEdge.java
+++ src/main/java/com/leaftaps/testcases/LoginEdge.java
@@ -26,32 +26,29 @@ public class LoginEdge {
 	@Test
 	public void loginTests() throws IOException, InterruptedException {
 
-
-		EdgeOptions options = new EdgeOptions(); 
-		DesiredCapabilities dc = new DesiredCapabilities(options); 
+		EdgeOptions options = new EdgeOptions();
+		DesiredCapabilities dc = new DesiredCapabilities(options);
 		dc.setBrowserName("MicrosoftEdge");
-		//dc.setVersion("120.0");
+		// dc.setVersion("120.0");
 		dc.setPlatform(Platform.LINUX);
 
-		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.40.48.160:4444/wd/hub"), dc);
+		RemoteWebDriver driver = new RemoteWebDriver(new URL("http://20.244.36.9:32000/wd/hub"), dc);
 
+		// ChromeDriver driver = new ChromeDriver();
 
-		// Step 2: Load an URL
-		driver.get("http://leaftaps.com/opentaps");
+		// Load the application url
+		driver.get("http://leaftaps.com/opentaps/control/main");
 
 		// Maximize the browser
 		driver.manage().window().maximize();
-
-		//Thread.sleep(10000);
-
 		// Step 3: Print the title --> getTitle
-		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
-		System.out.println("The title is "+driver.getTitle());
+				driver.findElement(By.id("username")).sendKeys("democsr");
+				System.out.println("The title is "+driver.getTitle());
 
-		Thread.sleep(5000);
+				Thread.sleep(5000);
 
-		// Step 3: Print the title --> getTitle
-		driver.findElement(By.id("password")).sendKeys("crmsfa");
+				// Step 3: Print the title --> getTitle
+				driver.findElement(By.id("password")).sendKeys("crmsfa");
 
 		// Click on login button
 		driver.findElement(By.className("decorativeSubmit")).click();
@@ -60,75 +57,62 @@ public class LoginEdge {
 
 		String text = driver.findElement(By.tagName("h2")).getText();
 		System.out.println(text);
-		
-		
+
 		// click on Crmsfa Link
 
 		driver.findElement(By.linkText("CRM/SFA")).click();
 
-		//To get the Title of the page
-		
+		// To get the Title of the page
+
 		String title = driver.getTitle();
 		System.out.println(title);
-		
+
 		if (title.contains("My Home")) {
 			System.out.println("verified Sucessfully");
 		} else {
 
 			System.out.println("Verified not Sucessfully");
 		}
-		
-		
-		
-		//Click on the Leads 
-		
-		driver.findElement(By.partialLinkText("Lea")).click();
-	
-		//CLick on the Create Leads Button
-		
-		driver.findElement(By.partialLinkText("Creat")).click();
-		
 
-		//Enter the Company Name
+		// Click on the Leads
+
+		driver.findElement(By.partialLinkText("Lea")).click();
+
+		// CLick on the Create Leads Button
+
+		driver.findElement(By.partialLinkText("Creat")).click();
+
+		// Enter the Company Name
 		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
-		
-		//Enter the First Name
+
+		// Enter the First Name
 		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Vinoth");
-		
-		//Enter The Last Name
+
+		// Enter The Last Name
 		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("S");
-		
-		
-		
-		
-		
-		
-		//Click on the Create Leads Button
+
+		// Click on the Create Leads Button
 		driver.findElement(By.name("submitButton")).click();
-		
-		
-		//To Verify the Lead First Name
-		
+
+		// To Verify the Lead First Name
+
 		WebElement findElement = driver.findElement(By.id("viewLead_firstName_sp"));
 		System.out.println(findElement.getText());
 		String firstName = findElement.getText();
 		if (firstName.equals("Vinoth")) {
-			
+
 			System.out.println("Verfied Sucessfully");
-			
+
 		} else {
 
 			System.out.println("Verified not Sucessfully");
-			
+
 		}
-		
-		
+
 		// To Get the Title of the Page
-		
+
 		String title2 = driver.getTitle();
 		System.out.println(title2);
-
-
 	}
 
 }
