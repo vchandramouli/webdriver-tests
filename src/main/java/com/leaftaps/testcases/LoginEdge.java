fe32cc75e9fb305eef86a5481016b52895ad5f02
Mohan Balakrishnan <38653431+mohan-balakrishnan@users.noreply.github.com>
2024-10-20 12:33:13 +0530

    Update LoginEdge.java

--- src/main/java/com/leaftaps/testcases/LoginEdge.java
+++ src/main/java/com/leaftaps/testcases/LoginEdge.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
